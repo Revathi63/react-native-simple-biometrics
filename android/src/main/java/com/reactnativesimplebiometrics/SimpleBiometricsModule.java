7f87fed386cb9ecb710bbb1cbd1385465703d9a0
Revathi63 <151715159+Revathi63@users.noreply.github.com>
2024-02-16 15:06:59 +0530

    Update SimpleBiometricsModule.java

--- android/src/main/java/com/reactnativesimplebiometrics/SimpleBiometricsModule.java
+++ android/src/main/java/com/reactnativesimplebiometrics/SimpleBiometricsModule.java
@@ -19,79 +19,116 @@ import androidx.fragment.app.FragmentActivity;
 
 
 @ReactModule(name = SimpleBiometricsModule.NAME)
-public class SimpleBiometricsModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "SimpleBiometrics";
 
-    static final int authenticators =  BiometricManager.Authenticators.BIOMETRIC_STRONG;
-  
-    public SimpleBiometricsModule(ReactApplicationContext reactContext) {
+public class SimpleBiometricsModule  extends ReactContextBaseJavaModule {
+
+    public SimpleBiometricsModule (ReactApplicationContext reactContext) {
         super(reactContext);
-    }
+          BiometricManager biometricManager = BiometricManager.from(reactContext);
 
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
-    }
-
-    @ReactMethod
-    public void canAuthenticate(Promise promise) {
-        try {
-            ReactApplicationContext context = getReactApplicationContext();
-            BiometricManager biometricManager = BiometricManager.from(context);
-
-            int res = biometricManager.canAuthenticate(authenticators);
-            boolean can = res == BiometricManager.BIOMETRIC_SUCCESS;
-
-            promise.resolve(can);
-        } catch (Exception e) {
-            promise.reject(e);
+        switch (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Log.d("MY_APP_TAG", "Biometric success");
+                // Biometric authentication is available
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Log.e("MY_APP_TAG", "No biometric features available on this device.");
+                // Device doesn't have biometric sensors
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Log.e("MY_APP_TAG", "Biometric features are currently unavailable.");
+                // Biometric features are currently unavailable
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Log.e("MY_APP_TAG", "The user hasn't enrolled any biometric credentials.");
+                // No biometric credentials enrolled
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED:
+                Log.e("MY_APP_TAG", "Biometric security update is required.");
+                // Biometric security update is required to use biometric authentication
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED:
+                Log.e("MY_APP_TAG", "Biometric authentication is not supported on this device.");
+                // Biometric authentication is not supported
+                break;
         }
+
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "SimpleBiometricsModule ";
     }
 
     @ReactMethod
-    public void requestBioAuth(final String title, final String subtitle, final Promise promise) {
-        UiThreadUtil.runOnUiThread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            ReactApplicationContext context = getReactApplicationContext();
-                            Activity activity = getCurrentActivity();
-                            Executor mainExecutor = ContextCompat.getMainExecutor(context);
-                            final BiometricPrompt.AuthenticationCallback authenticationCallback = new BiometricPrompt.AuthenticationCallback() {
-                                @Override
-                                public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                                    super.onAuthenticationError(errorCode, errString);
-                                    promise.reject(new Exception(errString.toString()));
-                                }
+    public void authenticate(final Promise promise) {
 
-                                @Override
-                                public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                                    super.onAuthenticationSucceeded(result);
-                                    promise.resolve(true);
-                                }
-                            };
+        
 
-                            if (activity != null) {
-                                BiometricPrompt prompt = new BiometricPrompt((FragmentActivity) activity, mainExecutor, authenticationCallback);
-
-                                BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                                        .setAllowedAuthenticators(authenticators)
-                                        .setTitle(title)
-                                        .setSubtitle(subtitle)
-                                        .build();
-
-                                prompt.authenticate(promptInfo);
-                            } else {
-                                throw new Exception("null activity");
-                            }
-                        } catch (Exception e) {
-                            promise.reject(e);
-                        }
-                    }
+        getCurrentActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Activity activity = getCurrentActivity();
+                if (activity == null) {
+                    promise.reject("ACTIVITY_NULL", "Activity is null");
+                    return;
                 }
-        );
+                FragmentActivity fragmentActivity = (FragmentActivity) activity;
 
+                AlertDialog.Builder builder = new AlertDialog.Builder(fragmentActivity);
+                builder.setTitle("Select Biometric Method")
+                        .setItems(new CharSequence[]{"Face", "Fingerprint"}, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                 // authenticateWithBiometric(BiometricManager.Authenticators.BIOMETRIC_WEAK, promise);
+                                if (which == 0) {
+                                   // Authenticate with fingerprint
+                                    authenticateWithBiometric(BiometricManager.Authenticators.BIOMETRIC_WEAK, promise);
+                                } else {
+                                   // Authenticate with face
+                                    authenticateWithBiometric(BiometricManager.Authenticators.BIOMETRIC_STRONG, promise);
+                                }
+                            }
+                        });
+                builder.show();
+            }
+        });
+    }
+
+    private void authenticateWithBiometric(int authenticatorType, Promise promise) {
+        FragmentActivity fragmentActivity = (FragmentActivity) getCurrentActivity();
+        BiometricPrompt.PromptInfo.Builder promptInfoBuilder = new BiometricPrompt.PromptInfo.Builder();
+        promptInfoBuilder.setTitle("Authenticate to unlock");
+        promptInfoBuilder.setSubtitle("");
+        promptInfoBuilder.setDescription("");
+        promptInfoBuilder.setNegativeButtonText("Cancel");
+        promptInfoBuilder.setAllowedAuthenticators(authenticatorType);
+
+        BiometricPrompt.PromptInfo promptInfo = promptInfoBuilder.build();
+
+        // Use FragmentActivity and its support library
+        BiometricPrompt biometricPrompt = new BiometricPrompt(fragmentActivity, fragmentActivity.getMainExecutor(), new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                promise.reject("AUTH_ERROR", errString.toString());
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                promise.resolve("Authenticated successfully");
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                promise.reject("AUTH_FAILED", "Authentication failed");
+            }
+        });
+
+        biometricPrompt.authenticate(promptInfo);
     }
 }
+
