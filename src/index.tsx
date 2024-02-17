9d680a6ac60da177448fac07c629e94d67b40b20
Revathi63 <151715159+Revathi63@users.noreply.github.com>
2024-02-17 10:52:27 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,12 @@ import { NativeModules } from 'react-native';
 
 const { SimpleBiometrics: RNBiometricsNative } = NativeModules;
 
-/**kkkkk
+/**
  * check if authentication is possible
  */
 const canAuthenticate = (): Promise<boolean> => {
-  return RNBiometricsNative.authenticate();
+  return RNBiometricsNative.canAuthenticate();
 };
-
 
 /**
  * request biometric authentication
@@ -16,25 +15,25 @@ const canAuthenticate = (): Promise<boolean> => {
  * note: promise will resolve when successful
  * but will be rejected when not with an error message
  */
-// const requestBioAuth = (
-//   /** title of prompt */
-//   promptTitle: string,
-//   /** The app-provided reason for requesting authentication, which displays in the authentication dialog presented to the user. */
-//   promptMessage: string
-// ): Promise<boolean> => {
-//   if (typeof promptTitle !== 'string' || !promptTitle) {
-//     throw new Error('prompt title must be a non empty string');
-//   }
+const requestBioAuth = (
+  /** title of prompt */
+  promptTitle: string,
+  /** The app-provided reason for requesting authentication, which displays in the authentication dialog presented to the user. */
+  promptMessage: string
+): Promise<boolean> => {
+  if (typeof promptTitle !== 'string' || !promptTitle) {
+    throw new Error('prompt title must be a non empty string');
+  }
 
-//   if (typeof promptMessage !== 'string' || !promptMessage) {
-//     throw new Error('prompt message must be a non empty string');
-//   }
+  if (typeof promptMessage !== 'string' || !promptMessage) {
+    throw new Error('prompt message must be a non empty string');
+  }
 
-//   return RNBiometricsNative.requestBioAuth(promptTitle, promptMessage);
-// };
+  return RNBiometricsNative.requestBioAuth(promptTitle, promptMessage);
+};
 
 const RNBiometrics = {
- // requestBioAuth,
+  requestBioAuth,
   canAuthenticate,
 };
 
